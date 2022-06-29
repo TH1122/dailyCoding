916efa7839818f712d0d1f5e6772206b6fe3bdef
TaeHyung KIM <th1122kim@gmail.com>
2022-06-29 22:18:46 +0900

    220629 daily coding

---
+++ leetcode/No.69_Sqrt(x)(easy)/ref.md
@@ -0,0 +1,9 @@
+1) ref: https://leetcode.com/problems/sqrtx/
+
+2) report : 
+
+Runtime: 210 ms, faster than 5.08% of JavaScript online submissions for Sqrt(x).
+
+Memory Usage: 42.6 MB, less than 94.11% of JavaScript online submissions for Sqrt(x).
+
+// -> Runtime 개선 필요
---
+++ leetcode/No.69_Sqrt(x)(easy)/script.js
@@ -0,0 +1,15 @@
+var mySqrt = function(x) {
+    if (x <= 1) {
+        return x;
+    }
+    else {
+        for (let i=2; i<=x; i++) {
+            if(i*i === x) {
+                return i;
+            }
+            else if (i*i > x) {
+                return i-1
+            }
+        }
+    }  
+};
