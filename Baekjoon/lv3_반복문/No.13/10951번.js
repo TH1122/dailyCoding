22fcef9ec9aaa76dfda8b82a34f562f6d8c98d76
TaeHyung KIM <th1122kim@gmail.com>
2022-07-14 16:58:31 +0900

    220714 daily coding

---
+++ Baekjoon/lv3_반복문/No.13/10951번.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let result = ""
+for (i=0; i<input.length; i++) {
+    let sum = 0;
+    input[i].trim().split(" ").map(Number).forEach(el => sum += el);
+    result += `${sum}\n`
+}
+console.log(result.trim())
