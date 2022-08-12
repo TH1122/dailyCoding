cd123b162265254dfd89c6b0b0c7d6d6c3d1c07a
TaeHyung KIM <th1122kim@gmail.com>
2022-08-12 21:25:41 +0900

    220812 daily coding

---
+++ Baekjoon/lv9_재귀/No.1/10872번.js
@@ -0,0 +1,8 @@
+const input = Number(require("fs").readFileSync("/dev/stdin").toString());
+const factorial = (num) => {
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
+console.log(factorial(input));
